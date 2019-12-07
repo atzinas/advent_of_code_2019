319874adc5ce5c69cac2d30ae766809a9f47dea0
tzinas <atzinas2001@gmail.com>
2019-12-08 01:11:49 +0200

    Added Day 1

---
+++ day_1/program_1.py
@@ -0,0 +1,13 @@
+def fuel(mass):
+	fuel_mass = mass // 3 - 2
+	if fuel_mass <= 0:
+		return 0
+	return fuel_mass + fuel(fuel_mass)
+
+with open("in.txt", "r") as f:
+	components = f.read().splitlines()
+
+components = map(int, components)
+fuel_per_component = map(fuel, components)
+
+print(sum(fuel_per_component))
