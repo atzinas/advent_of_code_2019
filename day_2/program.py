de721aa8794e1f5db61ec6fe04be9b36c03a484f
tzinas <atzinas2001@gmail.com>
2019-12-08 01:30:53 +0200

    Code Improvments

--- day_2/program.py
+++ day_2/program.py
@@ -1,34 +1,34 @@
-return_position = 0 
-add  = 1
-multiply = 2
-wanted_answer = 19690720
+RETURN_POSITION = 0 
+ADD = 1
+MULTIPLY = 2
+WANTED_ANSWER = 19690720
 
 with open("in.txt", "r") as f:
 	intcode_string = f.read()
 
 intcode_string = intcode_string.split(',')
 
-for number_in_position_1 in range(100):
-	for number_in_position_2 in range(100):
+for pos1 in range(100):
+	for pos2 in range(100):
 		intcode = list(map(int, intcode_string))
-		intcode[1] = number_in_position_1
-		intcode[2] = number_in_position_2
+		intcode[1] = pos1
+		intcode[2] = pos2
 		instruction_pointer = 0
 		while instruction_pointer < len(intcode):
-			first_number_position = intcode[instruction_pointer + 1]
-			second_number_position = intcode[instruction_pointer + 2]
+			candidate_noun = intcode[instruction_pointer + 1]
+			candidate_verb = intcode[instruction_pointer + 2]
 			result_position = intcode[instruction_pointer + 3]
-			if intcode[instruction_pointer] == add:
-				intcode[result_position] = intcode[first_number_position] + intcode[second_number_position]
-				instruction_pointer += 4
-			elif intcode[instruction_pointer] == multiply:
-				intcode[result_position] = intcode[first_number_position] * intcode[second_number_position]
-				instruction_pointer +=4 
+			if intcode[instruction_pointer] == ADD:
+				intcode[result_position] = intcode[candidate_noun] + intcode[candidate_verb]
+			elif intcode[instruction_pointer] == MULTIPLY:
+				intcode[result_position] = intcode[candidate_noun] * intcode[candidate_verb]
 			else:
 				break
+			
+			instruction_pointer += 4 
 	
-		if intcode[return_position] == wanted_answer:
-			noun = number_in_position_1
-			verb = number_in_position_2
+		if intcode[RETURN_POSITION] == WANTED_ANSWER:
+			noun = pos1
+			verb = pos2
 
-print(100 * noun +verb)
+print(100 * noun + verb)
