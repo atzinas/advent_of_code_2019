b744de389e5ded01029a921cd0b467d0bdac6cbf
tzinas <atzinas2001@gmail.com>
2019-12-08 16:34:35 +0200

    Fixed Tab

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,11 +1,11 @@
 def fuel(mass):
-	fuel_mass = mass // 3 - 2
-	if fuel_mass <= 0:
-		return 0
-	return fuel_mass + fuel(fuel_mass)
+    fuel_mass = mass // 3 - 2
+    if fuel_mass <= 0:
+        return 0
+    return fuel_mass + fuel(fuel_mass)
 
 with open("in.txt", "r") as f:
-	components = f.read().splitlines()
+    components = f.read().splitlines()
 
 components = map(int, components)
 fuel_per_component = map(fuel, components)
--- day_2/day_2.py
+++ day_2/day_2.py
@@ -4,31 +4,30 @@ MULTIPLY = 2
 WANTED_ANSWER = 19690720
 
 with open("in.txt", "r") as f:
-	intcode_string = f.read()
+    intcode_string = f.read()
 
 intcode_string = intcode_string.split(',')
 
 for pos1 in range(100):
-	for pos2 in range(100):
-		intcode = list(map(int, intcode_string))
-		intcode[1] = pos1
-		intcode[2] = pos2
-		instruction_pointer = 0
-		while instruction_pointer < len(intcode):
-			candidate_noun = intcode[instruction_pointer + 1]
-			candidate_verb = intcode[instruction_pointer + 2]
-			result_position = intcode[instruction_pointer + 3]
-			if intcode[instruction_pointer] == ADD:
-				intcode[result_position] = intcode[candidate_noun] + intcode[candidate_verb]
-			elif intcode[instruction_pointer] == MULTIPLY:
-				intcode[result_position] = intcode[candidate_noun] * intcode[candidate_verb]
-			else:
-				break
-			
-			instruction_pointer += 4 
+    for pos2 in range(100):
+        intcode = list(map(int, intcode_string))
+        intcode[1] = pos1
+        intcode[2] = pos2
+        instruction_pointer = 0
+        while instruction_pointer < len(intcode):
+            candidate_noun = intcode[instruction_pointer + 1]
+            candidate_verb = intcode[instruction_pointer + 2]
+            result_position = intcode[instruction_pointer + 3]
+            if intcode[instruction_pointer] == ADD:
+                intcode[result_position] = intcode[candidate_noun] + intcode[candidate_verb]
+            elif intcode[instruction_pointer] == MULTIPLY:
+                intcode[result_position] = intcode[candidate_noun] * intcode[candidate_verb]
+            else:
+                break
+            instruction_pointer += 4 
 	
-		if intcode[RETURN_POSITION] == WANTED_ANSWER:
-			noun = pos1
-			verb = pos2
+            if intcode[RETURN_POSITION] == WANTED_ANSWER:
+                noun = pos1
+                verb = pos2
 
 print(100 * noun + verb)
