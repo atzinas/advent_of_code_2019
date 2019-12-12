59db6a22f63b58aecbc041b3fe95f081bf11e89f
tzinas <atzinas2001@gmail.com>
2019-12-12 20:07:56 +0200

    Adds Day_5 Part_1 (room for improvement)

---
+++ day_5/day_5.py
@@ -0,0 +1,65 @@
+INPUT = 3
+OUTPUT = 4
+RETURN_POSITION = 0 
+ADD = 1
+MULTIPLY = 2
+END = 99
+WANTED_ANSWER = 19690720
+
+with open("in.txt", "r") as f:
+    intcode_string = f.read()
+
+intcode = intcode_string.split(',')
+
+instruction_pointer = 0
+while instruction_pointer < len(intcode) - 1:
+    mode = [0, 0, 0]
+    if len(intcode[instruction_pointer]) > 1:
+        instruction_size = len(intcode[instruction_pointer])
+        command = int(intcode[instruction_pointer][instruction_size - 2] + intcode[instruction_pointer][instruction_size - 1])
+        digit_pointer = instruction_size -3 
+        mode_pointer = 0
+        while digit_pointer >= 0:
+            mode[mode_pointer] = int(intcode[instruction_pointer][digit_pointer])
+            mode_pointer += 1
+            digit_pointer -= 1
+    else:
+        command = int(intcode[instruction_pointer])
+    if command == ADD:
+        pos_1 = int(intcode[instruction_pointer + 1])
+        pos_2 = int(intcode[instruction_pointer + 2])
+        pos_3 = int(intcode[instruction_pointer + 3])
+        instruction_pointer += 4
+        if mode[0] == 0:
+            value_1 = int(intcode[pos_1])
+        else:
+            value_1 = pos_1
+        if mode[1] == 0:
+            value_2 = int(intcode[pos_2])
+        else:
+            value_2 = pos_2
+        intcode[pos_3] = str(value_1 + value_2)
+    elif command  == MULTIPLY:
+        pos_1 = int(intcode[instruction_pointer + 1])
+        pos_2 = int(intcode[instruction_pointer + 2])
+        pos_3 = int(intcode[instruction_pointer + 3])
+        instruction_pointer += 4
+        if mode[0] == 0:
+            value_1 = int(intcode[pos_1])
+        else:
+            value_1 = pos_1
+        if mode[1] == 0:
+            value_2 = int(intcode[pos_2])
+        else:
+            value_2 = pos_2
+        intcode[pos_3] = str(value_1 * value_2) 
+    elif command == OUTPUT:
+        pos_1 = int(intcode[instruction_pointer + 1])
+        instruction_pointer += 2
+        print(intcode[pos_1])
+    elif command == INPUT:
+        pos_1 = int(intcode[instruction_pointer + 1])
+        instruction_pointer += 2
+        intcode[pos_1] = '1'
+    elif command == END:
+        break
