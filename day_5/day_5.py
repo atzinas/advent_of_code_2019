d5038144d31fcf34a6b9405299b7cd31cedb49b2
tzinas <atzinas2001@gmail.com>
2019-12-13 13:35:17 +0200

    Adds day_5 part_2 (room for improvement)

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,10 +1,13 @@
+JUMP_IF_TRUE = 5
+JUMP_IF_FALSE = 6
+LESS_THAN = 7
+EQUAL = 8
 INPUT = 3
 OUTPUT = 4
 RETURN_POSITION = 0 
 ADD = 1
 MULTIPLY = 2
 END = 99
-WANTED_ANSWER = 19690720
 
 with open("in.txt", "r") as f:
     intcode_string = f.read()
@@ -60,6 +63,68 @@ while instruction_pointer < len(intcode) - 1:
     elif command == INPUT:
         pos_1 = int(intcode[instruction_pointer + 1])
         instruction_pointer += 2
-        intcode[pos_1] = '1'
+        intcode[pos_1] = '5'
+    elif command == JUMP_IF_TRUE:
+        pos_1 = int(intcode[instruction_pointer + 1])
+        pos_2 = int(intcode[instruction_pointer + 2])
+        if mode[0] == 0:
+            value_1 = int(intcode[pos_1])
+        else:
+            value_1 = pos_1
+        if value_1 != 0:
+            if mode[1] == 0:
+                instruction_pointer = int(intcode[pos_2]) 
+            else:
+                instruction_pointer = pos_2
+        else:
+            instruction_pointer += 3
+    elif command == JUMP_IF_FALSE:
+        pos_1 = int(intcode[instruction_pointer + 1])
+        pos_2 = int(intcode[instruction_pointer + 2])
+        if mode[0] == 0:
+            value_1 = int(intcode[pos_1])
+        else:
+            value_1 = pos_1
+        if value_1 == 0:
+            if mode[1] == 0:
+                instruction_pointer = int(intcode[pos_2]) 
+            else:
+                instruction_pointer = pos_2
+        else:
+            instruction_pointer += 3
+    elif command == LESS_THAN:
+        pos_1 = int(intcode[instruction_pointer + 1])
+        pos_2 = int(intcode[instruction_pointer + 2])
+        pos_3 = int(intcode[instruction_pointer + 3])
+        if mode[0] == 0:
+            value_1 = int(intcode[pos_1])
+        else:
+            value_1 = pos_1
+        if mode[1] == 0:
+            value_2 = int(intcode[pos_2])
+        else:
+            value_2 = pos_2
+        if value_1 < value_2:
+            intcode[pos_3] = '1'
+        else:
+            intcode[pos_3] = '0'
+        instruction_pointer += 4
+    elif command == EQUAL:
+        pos_1 = int(intcode[instruction_pointer + 1])
+        pos_2 = int(intcode[instruction_pointer + 2])
+        pos_3 = int(intcode[instruction_pointer + 3])
+        if mode[0] == 0:
+            value_1 = int(intcode[pos_1])
+        else:
+            value_1 = pos_1
+        if mode[1] == 0:
+            value_2 = int(intcode[pos_2])
+        else:
+            value_2 = pos_2
+        if value_1 == value_2:
+            intcode[pos_3] = '1'
+        else:
+            intcode[pos_3] = '0'
+        instruction_pointer += 4
     elif command == END:
         break
